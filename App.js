7d37218229a926518a79fcc3358c75af6a749dee
Vilasagarapu-Harshitha <129965751+Vilasagarapu-Harshitha@users.noreply.github.com>
2024-07-02 12:22:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+// src/App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AllProductsPage from './components/AllProductsPage';
+import ProductDetailPage from './components/ProductDetailPage';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" exact>
+            <AllProductsPage />
+          </Route>
+          <Route path="/product/:productId">
+            <ProductDetailPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
